43b477a634ca27af2dfba4793022a6027b064ee3
Amanda-Arya <amandacvergilio.eng@hotmail.com>
2024-03-10 12:49:32 -0300

    Add project files.

---
+++ src/AIrline/AirlineWeb/Controllers/WebookSubscriptionController.cs
@@ -0,0 +1,21 @@
+﻿using AirlineWeb.Data;
+using AirlineWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirlineWeb.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class WebookSubscriptionController : ControllerBase
+{
+    private readonly AirlineDbContext _context;
+    public WebookSubscriptionController(AirlineDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult<WebhookSubscription> CreateSubscription(WebhookSubscription webhookSubscription)
+    {
+
+    }
+}
---
+++ src/AIrline/AirlineWeb/Dtos/WebookSubscriptionCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AirlineWeb.Dtos;
+
+public class WebookSubscriptionCreateDto
+{
+    public string WebhookURI { get; set; }
+    public string Secret { get; set; }
+    public string WebhookType { get; set; }
+    public string WebhookPublisher { get; set; }
+}
